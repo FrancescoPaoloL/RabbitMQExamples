e3a1b5379e511510567874a4843dc3cdfed09dc0
fpl <francescopaololezza@gmail.com>
2023-06-28 21:16:26 +0200

    Added date time calculator

--- src/DelayMessage/datetime_calculator.py
+++ src/DelayMessage/datetime_calculator.py
@@ -4,7 +4,7 @@ def calculate_delay_until_next_datetime(start_datetime):
     now = datetime.now()
     target_datetime = start_datetime
 
-    if target_datetime <= now:
+    if target_datetime > now:
         delta_days = (target_datetime.weekday() - now.weekday()) % 7
         target_datetime += timedelta(days=delta_days)
 
